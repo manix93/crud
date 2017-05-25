f4b121e738bd50ce0fd45c0489d8fb5f801eae55
Lukasz <manix93@gmail.com>
2017-05-25 17:01:01 +0200

    Add posts resources and root posts/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-
-  get 'posts/edit'
-
-  get 'posts/index'
-
-  get 'posts/show'
+  resources :posts
+  root 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
