c22e5a30ffad53bb235cdd143ee45c138804bfc7
Lukasz <manix93@gmail.com>
2017-05-25 17:20:42 +0200

    Add session author

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
   def new
-    @post = Post.new
+    @post = Post.new(author: session[:author])
   end
 
   def create
     @post = Post.new(post_params)
     @post.save
+    session[:author] = @post.author
     flash[:notice] = "Post został dodany pomyślnie"
     redirect_to posts_path
   end
